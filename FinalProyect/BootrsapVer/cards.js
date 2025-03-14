409e1d0ce588d61e6563b9c73decb407486271c0
Danxer <joelcorona27.dc@gmail.com>
2025-03-14 17:17:42 -0600

    Update cards.js

--- FinalProyect/BootrsapVer/cards.js
+++ FinalProyect/BootrsapVer/cards.js
@@ -1,5 +1,6 @@
 
 class Serie {
+    tag = 0;    
     title = "";
     totalEsisodes = 0;
     numSeaons = 0;
@@ -16,7 +17,8 @@ class Serie {
     dislikeInteractions = 0;
     streamingSite = []
 
-    constructor(n, tE, nS, rS, mS, sin, st, dob, pt, tl, pl, SS){
+    constructor(tag, n, tE, nS, rS, mS, sin, st, dob, pt, tl, pl, SS){
+        this.tag = tag;
         this.title = n
         this.totalEsisodes = tE;
         this.numSeaons = nS;
@@ -39,9 +41,9 @@ class Serie {
     }
 }
 
-anim = new Serie("The emminence in shadow", 32,  2, "Fall", 8.5, "Es un Isekai xd", "Nexus", true, "Netflix", "none", "https://images.justwatch.com/poster/301091057/s718/the-eminence-in-shadow.jpg", "https://www.netflix.com/watch/81642098?source=35");
+anim = new Serie(0, "The emminence in shadow", 32,  2, "Fall", 8.5, "Es un Isekai xd", "Nexus", true, "Netflix", "none", "https://images.justwatch.com/poster/301091057/s718/the-eminence-in-shadow.jpg", "https://www.netflix.com/watch/81642098?source=35");
 anim.posterLink = "https://images.justwatch.com/poster/301091057/s718/the-eminence-in-shadow.jpg";
-anim2 = new Serie("The apothecary Daires", 36, 2, "Fall", 9.1, "Los diarios chidos de la boticaria", "Chido", true, "Crunchyroll", "https://www.youtube.com/watch?v=XYNGkSvFT8c&t=19s", "https://m.media-amazon.com/images/M/MV5BNjAxMmFjZjgtYjM1ZS00NzdmLTliZDktZmIyMzU5YTBlNDBmXkEyXkFqcGc@._V1_.jpg","https://www.crunchyroll.com/es/series/G3KHEVDJ7/the-apothecary-diaries")
+anim2 = new Serie(1, "The apothecary Daires", 36, 2, "Fall", 9.1, "Los diarios chidos de la boticaria", "Chido", true, "Crunchyroll", "https://www.youtube.com/watch?v=XYNGkSvFT8c&t=19s", "https://m.media-amazon.com/images/M/MV5BNjAxMmFjZjgtYjM1ZS00NzdmLTliZDktZmIyMzU5YTBlNDBmXkEyXkFqcGc@._V1_.jpg","https://www.crunchyroll.com/es/series/G3KHEVDJ7/the-apothecary-diaries")
 recentSeries = [];
 
 recentSeries.push(anim);
